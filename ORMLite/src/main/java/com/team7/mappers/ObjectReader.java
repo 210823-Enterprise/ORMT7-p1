9741afa3f69c2a71dba9003a0e17a9bf39862056
Noah Gaston <nrgman@tds.net>
2021-09-25 15:11:38 -0500

    ugh

--- ORMLite/src/main/java/com/team7/mappers/ObjectReader.java
+++ ORMLite/src/main/java/com/team7/mappers/ObjectReader.java
@@ -1,5 +1,5 @@
 package com.team7.mappers;
-
+import org.apache.log4j.Logger;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -19,8 +19,10 @@ public class ObjectReader {
 	{
 		super();
 	}
+	private static Logger log = Logger.getLogger(ObjectReader.class);
 	public boolean readFromDb(Class<?> obj, Connection conn, Map<String, String> cont)
 	{
+		log.info("Searching based on input parameters.");
 		MetaModel<?> metaknight = MetaModel.of(obj);
 		String sql = "SELECT * FROM " + metaknight.getTableName() + " WHERE";
 		boolean success = false;
@@ -30,6 +32,7 @@ public class ObjectReader {
 		{
 			if(cont.containsKey(yub.getColumnName()) == true)
 			{
+				log.info("Contains key: " + yub.getColumnName());
 				sql = sql + " " + yub.getColumnName() + " = '" + cont.get(yub.getColumnName()) + "'";
 				if(col < cont.size())
 				{
@@ -42,26 +45,38 @@ public class ObjectReader {
 		try
 		{
 			PreparedStatement stmt = conn.prepareStatement(sql);
+			log.info("Attempting to execute statement.");
 			success = stmt.execute();
+			log.info("Success");
 			ResultSet rs = stmt.executeQuery();
 			while(rs.next())
 			{
+				log.info("Searching through result set.");
 				ResultSetMetaData rsmeta = rs.getMetaData();
 				for(int i = 1; i <= rsmeta.getColumnCount(); i++)
 				{
 					System.out.println(rsmeta.getColumnName(i)+ ": " + rs.getObject(i));
+					log.info("printing object and value.");
 				}
 			}
 		}
 		catch(SQLException e)
 		{
+			log.warn("Failed to deal with prepared statement.");
 			e.printStackTrace();
 		}
+		catch(NullPointerException e)
+		{
+			log.warn("Out of bounds. " + e);
+		}
+		log.info("ending method.");
 		return success;
 	}
 	public boolean readPrimaryKey(Class<?> obj, Connection conn, Map<String, String> cont)
 	{
+		log.info("Getting data based on primary key.");
 		MetaModel<?> metaknight = MetaModel.of(obj);
+		log.info("Metamodel created.");
 		String sql = "SELECT * FROM " + metaknight.getTableName() + " WHERE " + metaknight.getPrimaryKey().getColumnName() +
 				" = " + cont.get(metaknight.getPrimaryKey().getName());
 		boolean success = false;
@@ -69,13 +84,17 @@ public class ObjectReader {
 		sql += ";";
 		try
 		{
+			log.info("Executing statement.");
 			PreparedStatement stmt = conn.prepareStatement(sql);
 			success = stmt.execute();
+			log.info(stmt + " successfully executed.");
 			ResultSet rs = stmt.executeQuery();
 			while(rs.next())
 			{
-				System.out.println("yub");
+				log.info("Getting result set meta data.");
 				ResultSetMetaData rsmeta = rs.getMetaData();
+				log.info("Data obtained.");
+				log.info("Printing columns and values.");
 				for(int i = 1; i <= rsmeta.getColumnCount(); i++)
 				{
 					System.out.println(rsmeta.getColumnName(i)+ ": " + rs.getObject(i));
@@ -85,11 +104,17 @@ public class ObjectReader {
 		catch(SQLException e)
 		{
 			e.printStackTrace();
+			log.warn("Something happenned." + e);
+		}
+		catch(NullPointerException e)
+		{
+			log.warn("Empty parameter." + e);
 		}
 		return success;
 	}
-	public Map<String, String> getEntry(Class<?> obj, Connection conn, int ki)
+	public Map<String, String> getEntry(Class<?> obj, Connection conn, Object ki)
 	{
+		log.info("Getting columns of specified primary key.");
 		Map<String, String> ret = new HashMap<String, String>();
 		MetaModel<?> meta = MetaModel.of(obj);
 		String sql = "SELECT * FROM " + meta.getTableName() + " WHERE " + meta.getPrimaryKey().getColumnName() + " = " + ki;
