1e0208fe440e1fc2ed7c8de3faa0185e77db8bc9
Foxpope <48967406+Foxpope@users.noreply.github.com>
2021-09-26 15:27:31 -0400

    updated foreign key
    now has getFieldValue and getSQLType

--- ORMLite/src/main/java/com/team7/util/ForeignKeyField.java
+++ ORMLite/src/main/java/com/team7/util/ForeignKeyField.java
@@ -27,4 +27,36 @@ public class ForeignKeyField {
         return field.getAnnotation(JoinColumn.class).columnName();
     }
 
+	//TODO make this inherited or from util class (try to only write it once)
+	public String getSQLType() {
+		String type = field.getType().getSimpleName();
+		
+		String retVal; //return type string
+		switch (type) {
+		case "int":
+		case "Integer":
+			retVal = "integer";
+			break;
+		case "String":
+			retVal = "varchar(250)";
+			break;
+		default :
+			retVal = "varchar(10)"; //type not found TODO put log here
+		}
+		
+		return retVal;
+	}
+	
+	public String getFieldValue(Object o) {
+		try {
+			field.setAccessible(true);
+			//surround with single quotes for varchar
+			return (field.getType().getSimpleName().equals("String") ? "'" + field.get(o).toString() + "'" : field.get(o).toString());
+		} catch (SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			// TODO logs
+			e.printStackTrace();
+			return null;
+		}
+	}
+    
 }
